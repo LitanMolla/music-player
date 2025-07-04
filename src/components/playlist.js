2f89e7da58c1f1e56fe8ab6ad4e2eb5c16d191be
Dsx7 <79498453+Dsx7@users.noreply.github.com>
2025-07-04 23:06:50 +0600

    added song

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -1,7 +1,7 @@
 const playlist = [
-  { id: 1, title: "Ahista Ahista", artist: "Kazinama", duration: "2:57", url: "/tracks/Kazinama-Ahista-Ahista.mp3" },
-  { id: 2, title: "Kiven Mukhre", artist: "Kazinama", duration: "3:20", url: "/tracks/Kazinama-Kiven-Mukhre.mp3" },
-  { id: 3, title: "Tauba Tauba", artist: "Kazinama", duration: "3:55", url: "/tracks/Kazinama-Tauba-Tauba.mp3" },
+  { id: 1, title: "LOKE BOLE - Slowed Reverb", artist: "Bayazid Hasan", duration: "3:48", url: 'https://bhoot.cam/files/LOKE_BOLE.mp3' },
+  { id: 2, title: "Sanjida", artist: "Bayazid Hasan", duration: "3:09", url: 'https://bhoot.cam/files/Sanjida.mp3' },
+  { id: 3, title: "Rise and Shine", artist: "Bayazid Hasan, Dsx Spa", duration: "2:22", url: "/tracks/Kazinama-Tauba-Tauba.mp3" },
   { id: 4, title: "Soda Water", artist: "Kazinama", duration: "1:39", url: 'https://www.kazinama.com/tracks/Kazinama-Soda-Water.mp3' },
   { id: 5, title: "Kabhi Tanhayion Main Bhi", artist: "Kazinama", duration: "3:54", url: 'https://www.kazinama.com/tracks/Kazinama-Kabhi-Tanhayion-Main-bhi.mp3' },
   { id: 6, title: "Bayazid Er biye", artist: "Sirajganj", duration: "12:54", url: '' },
